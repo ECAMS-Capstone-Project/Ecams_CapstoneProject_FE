72cce8a4171534140e51cb508424b3a2e38aebf6
thanhvann <tvtvan1407@gmail.com>
2025-01-19 23:56:41 +0700

    login form

---
+++ src/pages/authentication/login.tsx
@@ -0,0 +1,13 @@
+import Header from "@/components/layout/header";
+import LoginForm from "@/components/partial/auth/loginForm";
+
+const Login = () => {
+  return (
+    <>
+      <Header />
+      <LoginForm />
+    </>
+  );
+};
+
+export default Login;
