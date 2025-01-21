888c1aa5571b036fd9b109d19a6637033db7b913
hungvietle227 <leviethung220703@gmail.com>
2025-01-21 17:12:39 +0700

    [Fix]_Fix conflict

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes";
 import { Toaster } from 'react-hot-toast';
+import React from "react";
 
 function App() {
   return (
--- src/pages/authentication/login.tsx
+++ src/pages/authentication/login.tsx
@@ -1,8 +1,10 @@
+import Header from "@/components/layout/header";
 import LoginForm from "@/components/partial/auth/loginForm";
 
 const Login = () => {
   return (
     <>
+      <Header />
       <LoginForm />
     </>
   );
