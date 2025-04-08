d85456ed1e5a00b1b563ba6e0d2e1055062515e5
thanhvann <tvtvan1407@gmail.com>
2025-04-09 00:07:32 +0700

    [Fix]_fix extend cancel contract

--- src/models/Payment.ts
+++ src/models/Payment.ts
@@ -1,43 +1,43 @@
 export interface PaymentDetails {
-    accountNumber: string;
-    amount: number;
-    bin: string;
-    checkoutUrl: string;
-    currency: string;
-    description: string;
-    expiredAt: string | null;
-    orderCode: number;
-    paymentLinkId: string;
-    qrCode: string;
-    status: string;
+  accountNumber: string;
+  amount: number;
+  bin: string;
+  checkoutUrl: string;
+  currency: string;
+  description: string;
+  expiredAt: string | null;
+  orderCode: number;
+  paymentLinkId: string;
+  qrCode: string;
+  status: string;
 }
 export interface HandleResponse {
-    representativeId: string;
-    transactionInfo: string;
-    transactionNumber: string;
-    isSuccess: boolean;
+  representativeId: string;
+  transactionInfo: string;
+  transactionNumber: string;
+  isSuccess: boolean;
 }
 export interface CheckBuyPackage {
-    packageId: string;
-    representativeId: string;
+  packageId: string;
+  representativeId: string;
 }
 export interface Transaction {
-    transactionId: string;
-    amount: number;
-    paymentDate: string; // ISO 8601 format
-    universityName: string;
-    methodName: string;
-    packageName: string;
-    transactionNumber: string;
-    transactionInfo: string;
-    contractUrl: string;
-    type: string; // Enum nếu có danh sách cụ thể
-    status: string;
+  transactionId: string;
+  amount: number;
+  paymentDate: string; // ISO 8601 format
+  universityName: string;
+  methodName: string;
+  packageName: string;
+  transactionNumber: string;
+  transactionInfo: string;
+  contractUrl: string;
+  type: string; // Enum nếu có danh sách cụ thể
+  status: string;
 }
 
 export interface StudentHandleResponse {
-    studentId: string;
-    transactionInfo: string;
-    transactionNumber: string;
-    isSuccess: boolean;
+  studentId: string;
+  transactionInfo: string;
+  transactionNumber: string;
+  isSuccess: boolean;
 }
