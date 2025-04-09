ab0978c4e0ec6dab706bc0e845e0f84272225ffb
thanhvann <tvtvan1407@gmail.com>
2025-04-09 13:54:28 +0700

    [Fix]_fix waiting payos event

--- src/models/Auth/UserAuth.ts
+++ src/models/Auth/UserAuth.ts
@@ -1,15 +1,16 @@
 export interface UserAuthDTO {
-    userId: string;
-    email: string;
-    fullname: string;
-    avatar: string;
-    isVerified: boolean;
-    universityId?: string;
-    roles: string[];
-    representativeId: string;
-    universityStatus: string;
-    universityName: string
-    phonenumber: string;
-    status: string;
-    isRecommended: boolean;
+  userId: string;
+  email: string;
+  fullname: string;
+  avatar: string;
+  isVerified: boolean;
+  universityId?: string;
+  roles: string[];
+  representativeId: string;
+  universityStatus: string;
+  universityName: string;
+  phonenumber: string;
+  status: string;
+  isRecommended: boolean;
+  userType: string;
 }
