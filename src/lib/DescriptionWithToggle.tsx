d628a90f09145055e2e029a9f31c4b2c82381f63
hungvietle227 <leviethung220703@gmail.com>
2025-04-22 01:36:13 +0700

    [Update]_Update logic description with toggle

--- src/lib/DescriptionWithToggle.tsx
+++ src/lib/DescriptionWithToggle.tsx
@@ -1,28 +1,19 @@
 import React, { useState } from "react";
 import { Typography, Button } from "@mui/material";
 import { MoreHorizontal } from "lucide-react";
+import parse from "html-react-parser";
 
 interface DescriptionProps {
   text: string;
 }
 
 export const DescriptionWithToggle: React.FC<DescriptionProps> = ({ text }) => {
-  // Giới hạn số từ muốn hiển thị ban đầu
   const WORD_LIMIT = 20;
-
-  // Tách mô tả thành mảng từ
-  const words = text.split(" ");
-
-  // Kiểm tra xem có vượt quá giới hạn hay không
+  const words = text.replace(/<[^>]*>/g, "").split(" ");
   const isOverLimit = words.length > WORD_LIMIT;
-
-  // Tạo state để kiểm soát hiển thị đầy đủ hay không
   const [isExpanded, setIsExpanded] = useState(false);
 
-  // Nếu chưa expanded, cắt mảng words thành WORD_LIMIT, nếu expanded thì hiển thị tất cả
-  const displayedText = isExpanded
-    ? text
-    : words.slice(0, WORD_LIMIT).join(" ");
+  const displayedText = isExpanded ? text : words.slice(0, WORD_LIMIT).join(" ");
 
   return (
     <div>
@@ -32,8 +23,7 @@ export const DescriptionWithToggle: React.FC<DescriptionProps> = ({ text }) => {
         style={{ textAlign: "justify" }}
         className="text-gray-700 text-ba"
       >
-        {displayedText}
-        {/* Nếu chưa expanded và có nhiều hơn WORD_LIMIT từ => thêm "..." */}
+        {isExpanded ? parse(text) : displayedText}
         {!isExpanded && isOverLimit && " ..."}
       </Typography>
 
@@ -46,7 +36,6 @@ export const DescriptionWithToggle: React.FC<DescriptionProps> = ({ text }) => {
             e.stopPropagation();
             setIsExpanded(!isExpanded);
           }}
-          // endIcon={<MoreHorizontal size={16} />}
           sx={{ textTransform: "none", padding: 0, minWidth: "20px" }}
         >
           {isExpanded ? "Show less" : <MoreHorizontal size={16} />}
