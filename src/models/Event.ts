a5233996ee2b11f330372811d6c6e779200fc882
thanhvann <tvtvan1407@gmail.com>
2025-04-29 21:56:55 +0700

    [Fix]_fix filter inter event

--- src/models/Event.ts
+++ src/models/Event.ts
@@ -84,7 +84,7 @@ export interface InterClubEventDTO {
   trainingPoint: number;
   clubs: InterClub[]; // Danh sách câu lạc bộ liên quan đến sự kiện
   eventAreas?: EventAreas[]; // Các khu vực sự kiện
-  status: "ACTIVE" | "INACTIVE" | "PENDING" | "WAITING" | "ENDED";
+  status: "ACTIVE" | "INACTIVE" | "PENDING" | "WAITING" | "ENDED" | "CANCELED";
   startDate: Date;
   endDate: Date;
 }
