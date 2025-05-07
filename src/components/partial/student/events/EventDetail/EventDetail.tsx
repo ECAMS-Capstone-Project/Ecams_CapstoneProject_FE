1ca443758f6cd041d2c4841c414dc8116c4a4168
hungvietle227 <leviethung220703@gmail.com>
2025-05-07 10:50:22 +0700

    [Update]_logic code

--- src/components/partial/student/events/EventDetail/EventDetail.tsx
+++ src/components/partial/student/events/EventDetail/EventDetail.tsx
@@ -60,6 +60,7 @@ export const StudentEventDetail: React.FC = () => {
               eventId={eventId}
               studentId={user.userId}
               isEventEnded={isEventEnded}
+              event={event}
             />
           </div>
         )}
