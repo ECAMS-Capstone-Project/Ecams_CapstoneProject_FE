ee95b186d23c150ed0d7a247b336ddabc17a7030
hungvietle227 <leviethung220703@gmail.com>
2025-05-08 13:35:41 +0700

    [Update]_Validation

--- src/components/partial/auth/registerUniversityForm.tsx
+++ src/components/partial/auth/registerUniversityForm.tsx
@@ -27,7 +27,10 @@ const schema = z
   .object({
     fullName: z.string().min(3, "First name must be at least 3 characters"),
     email: z.string().email("Invalid email address"),
-    phoneNumber: z.string().min(10, "Phone number must be at least 10 digits"),
+    phoneNumber: z
+      .string()
+      .length(10, "Phone number must be exactly 10 digits")
+      .regex(/^\d{10}$/, "Phone number must contain only digits"),
     password: z.string().min(8, "Password must be at least 8 characters"),
     confirmPassword: z.string().min(8, "Confirm Password is required"),
     address: z.string().min(1, "Address is required"),
